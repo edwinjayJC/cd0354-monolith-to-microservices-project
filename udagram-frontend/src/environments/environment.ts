cc0d07c71fed6a99ee721ce52011c2ef80e332b6
Edwin Jay <edwinj@mit.edu>
2022-11-04 18:05:12 +0200

    Updated files

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a68c1a969c5d24cf69beb0b89356ef66-1280850208.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
